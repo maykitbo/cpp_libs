9f56d46d92c2157d4f8a3022659087af11053a52
Ivan <zakharovivann@gmail.com>
2023-06-11 21:24:49 +0300

    some refactor

--- ThreadPool/ThreadPool.h
+++ ThreadPool/ThreadPool.h
@@ -10,27 +10,12 @@ public:
 
   ThreadPool(size_t size) : stopped(false) {
     for (size_t i = 0; i < size; ++i) {
-      threads.emplace_back([this] {
-        std::function<void()> task;
-        while (true) {
-          std::unique_lock<std::mutex> lock(queueMutex);
-          condition.wait(lock, [this] {return stopped || !tasks.empty();});
-          if (stopped && tasks.empty())
-            return;
-          task = std::move(tasks.front());
-          tasks.pop();
-          try {
-            task();
-          } catch (std::runtime_error &ex) {
-            std::cout << "at " << std::this_thread::get_id() << " " << ex.what() << std::endl;
-          }
-        }
-      });
+      threads.emplace_back(std::thread(&ThreadPool::ThreadRun, this));
     }
   }
 
   template<class F, class... Args>
-  void enqueue(F&& f, Args&&... args) {
+  void AddTask(F&& f, Args&&... args) {
     std::unique_lock<std::mutex> lock(queueMutex);
     tasks.emplace([=] {f(args...);});
 
@@ -39,6 +24,24 @@ public:
 
 
 private:
+
+  void ThreadRun() {
+    std::function<void()> task;
+    while (true) {
+      std::unique_lock<std::mutex> lock(queueMutex);
+      condition.wait(lock, [this] {return stopped || !tasks.empty();});
+      if (stopped && tasks.empty())
+        return;
+      task = std::move(tasks.front());
+      tasks.pop();
+      try {
+        task();
+      } catch (std::runtime_error &ex) {
+        std::cout << "at " << std::this_thread::get_id() << " " << ex.what() << std::endl;
+      }
+    }
+  }
+
   std::mutex queueMutex;
   std::vector<std::thread> threads;
   std::queue<std::function<void()>> tasks;
