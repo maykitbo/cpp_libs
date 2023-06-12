4134836d72e453ca1ba72e173a6d960a3ae696f3
Ivan <zakharovivann@gmail.com>
2023-06-12 23:08:45 +0300

    add waiting for complete

--- thread_pool/thread_pool.h
+++ thread_pool/thread_pool.h
@@ -8,32 +8,53 @@ class ThreadPool {
 public:
   ThreadPool() : ThreadPool(std::thread::hardware_concurrency()) {}
 
-  ThreadPool(size_t size) : stopped(false) {
-    for (size_t i = 0; i < size; ++i) {
-      threads.emplace_back(std::thread(&ThreadPool::ThreadRun, this));
+  explicit ThreadPool(unsigned size) : stopped(false) {
+    for (int i = 0; i < size; ++i) {
+      threads.emplace_back(&ThreadPool::ThreadRun, this);
+    }
+  }
+
+  ~ThreadPool() {
+    if (!stopped) {
+      ShutDown();
     }
   }
 
   template<class F, class... Args>
   void AddTask(F&& f, Args&&... args) {
-    {
-      std::unique_lock<std::mutex> lock(queueMutex);
-      tasks.emplace([=] { f(args...); });
-    }
-    condition.notify_one();
+    AddTask([=] { f(args...); });
   }
 
-  void AddTask(std::function<void()> f) {
+  void AddTask(const std::function<void()>& f) {
     {
       std::unique_lock<std::mutex> lock(queueMutex);
       tasks.push(f);
+      ++active;
     }
     condition.notify_one();
   }
 
+  void ShutDown() {
+    stopped = true;
+    condition.notify_all();
+
+    for (std::thread &thread : threads) {
+      thread.join();
+    }
+    threads.clear();
+    stopped = false;
+  }
+
+  void WaitForComplete() {
+    std::unique_lock<std::mutex> lock(queueMutex);
+    condition.wait(lock, [this] {return !active;});
+    if (!active) {
+      condition.notify_one();
+      WaitForComplete();
+    }
+  }
 
 private:
-
   void ThreadRun() {
     std::function<void()> task;
     while (true) {
@@ -50,7 +71,11 @@ private:
       } catch (std::runtime_error &ex) {
         std::cout << "at " << std::this_thread::get_id() << " " << ex.what() << std::endl;
       }
+      if (active)
+        --active;
+      condition.notify_all();
     }
+
   }
 
   std::mutex queueMutex;
@@ -58,4 +83,5 @@ private:
   std::queue<std::function<void()>> tasks;
   std::condition_variable condition;
   std::atomic_bool stopped;
+  std::atomic_uint active{};
 };
