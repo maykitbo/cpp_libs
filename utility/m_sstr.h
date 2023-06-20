fb2e5b8b6075041b63ae4da36cc78e3b0d9424f3
Alexey <zam7@icloud.com>
2023-06-20 17:02:05 +0200

    print update

--- utility/m_sstr.h
+++ utility/m_sstr.h
@@ -27,15 +27,23 @@ struct SStr {
   template <const char Delimiter = '\n', class... Args >
   static void Print(const Args &... args) {
     int i = 0;
-    ([&] (const auto &arg) {
-      Print(std::cout, args);
-      std::cout << (++i == sizeof...(args) ? '\0' : Delimiter);
-    } (args), ...);
+    ([&] {
+      PrintP(args);
+      PrintStyle::stream << (++i == sizeof...(args) ? '\0' : Delimiter);
+    } (), ...);
+    PrintStyle::stream << '\n';
+  }
+
+  template <class T>
+  static void Print(const T &arg) {
+    PrintP(arg);
+    PrintStyle::stream << '\n';
   }
 
   struct PrintStyle {
     inline static std::string container_delimiter = ", ";
     inline static std::string pair_delimiter = ", ";
+    inline static std::ostream &stream = std::cout;
 
     template<class T>
     static typename std::enable_if<std::is_same<T, std::string>::value, std::string>::type
@@ -50,58 +58,74 @@ struct SStr {
     }
   };
 
-  template <typename T>
-  struct is_container {
-    template <typename U>
-    static constexpr bool is_container_helper(
-        U*,
-        decltype(std::declval<U>().begin())* = nullptr,
-        decltype(std::declval<U>().end())* = nullptr) {
-      return true;
+  private:
+
+    template <typename T>
+    struct is_container {
+      template <typename U>
+      static constexpr bool is_container_helper(
+          U*,
+          decltype(std::declval<U>().begin())* = nullptr,
+          decltype(std::declval<U>().end())* = nullptr) {
+        return true;
+      }
+
+      template <typename>
+      static constexpr bool is_container_helper(...) {
+        return false;
+      }
+
+      static constexpr bool value = is_container_helper<T>(nullptr);
+    };
+
+    template <class Container>
+    static typename std::enable_if<is_container<Container>::value, void>::type
+    PrintP(const Container &c) {
+      auto i = c.begin();
+      if (i == c.end()) {
+        return;
+      }
+      PrintStyle::stream << '{';
+      while (i != --c.end()) {
+        PrintP(*(i++));
+        PrintStyle::stream << PrintStyle::container_delimiter;
+      }
+      PrintP(*(i++));
+      PrintStyle::stream << "}";
     }
 
-    template <typename>
-    static constexpr bool is_container_helper(...) {
-      return false;
+    template<class T>
+    static typename std::enable_if<!is_container<T>::value, void>::type
+    PrintP(const T &data) {
+      PrintStyle::stream << data;
     }
 
-    static constexpr bool value = is_container_helper<T>(nullptr);
-  };
-
-  template <class Container>
-  static typename std::enable_if<is_container<Container>::value, void>::type
-  Print(std::ostream &os, const Container &c) {
-    auto i = c.begin();
-    if (i == c.end()) {
-      return;
+    template<class A, class B>
+    static void PrintP(const std::pair<A, B> &p) {
+      // PrintStyle::stream << '[' << p.first << PrintStyle::pair_delimiter << p.second << "]";
+      PrintStyle::stream << '[';
+      PrintP(p.first);
+      PrintStyle::stream << PrintStyle::pair_delimiter;
+      PrintP(p.second);
+      PrintStyle::stream << "]";
     }
-    os << '{';
-    while (i != --c.end()) {
-      os << PrintStyle::Wrapper(*(i++)) << PrintStyle::container_delimiter;
+
+    template<class A>
+    static void PrintP(const std::pair<A, A> &p) {
+      // PrintStyle::stream << '[' << p.first << PrintStyle::pair_delimiter << p.second << "]";
+      PrintStyle::stream << '[';
+      PrintP(p.first);
+      PrintStyle::stream << PrintStyle::pair_delimiter;
+      PrintP(p.second);
+      PrintStyle::stream << "]";
     }
-    os << PrintStyle::Wrapper(*i) << "}\n";
-  }
+  
+  public:
 
-  template<class T>
-  static typename std::enable_if<!is_container<T>::value, void>::type
-  Print(std::ostream &os, const T &data) {
-    os << data;
-  }
-
-  template<class A, class B>
-  static void Print(std::ostream &os, const std::pair<A, B> &p) {
-    os << '[' << p.first << PrintStyle::pair_delimiter << p.second << "]\n";
-  }
-
-  template<class A>
-  static void Print(std::ostream &os, const std::pair<A, A> &p) {
-    os << '[' << p.first << PrintStyle::pair_delimiter << p.second << "]\n";
-  }
-
-  static std::string RelativePath(const char *FILE, const std::string &path_from_source) {
-    using namespace std::filesystem;
-    return relative(path(FILE).parent_path(), current_path()).string() + path_from_source;
-  }
+    static std::string RelativePath(const char *FILE, const std::string &path_from_source) {
+      using namespace std::filesystem;
+      return relative(path(FILE).parent_path(), current_path()).string() + path_from_source;
+    }
 };
 
 } // namespace s21
